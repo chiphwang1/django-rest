b2e6ed0ac0df5d61d77123cba43b0264ec8a2588
chiphwang1 <64228847+chiphwang1@users.noreply.github.com>
2022-02-07 09:12:19 -0800

    added oracle_db in settings.py

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': os.getenv('db_name'),
+        'USER':  os.getenv('db_user'),
+        'PASSWORD': os.getenv('db_password'),
     }
 }
 
