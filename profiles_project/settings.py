fe3b4d7409fb9cbee8ccadf50f860dea6f11ed98
chiphwang1 <64228847+chiphwang1@users.noreply.github.com>
2022-02-07 23:41:21 -0800

    Update settings.py

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xfzz(3d_lsbk58e)t=wjcxhs_747tr80#5izu&%5h9+$x0y^q4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
